3d8aa5ee3f07483ec9b0e4fbde5230988d362dab
denn <shuvalov.1243@gmail.com>
2021-12-27 11:46:04 +0300

    finish

---
+++ local/php_interface/migrations/programs20211227114524.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ds\Migrate;
+
+
+class programs20211227114524 extends Version
+{
+    protected $description = "";
+
+    /**
+     * @throws Exceptions\ExchangeException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+            ->IblockElementsImport()
+            ->setExchangeResource('iblock_elements.xml')
+            ->setLimit(20)
+            ->execute(function ($item) {
+                $this->getHelperManager()
+                    ->Iblock()
+                    ->addElement(
+                        $item['iblock_id'],
+                        $item['fields'],
+                        $item['properties']
+                    );
+            });
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
