56ed75967b17b760a6564dab3070fb467c589c4c
denn <shuvalov.1243@gmail.com>
2022-02-07 02:39:49 +0300

    finish student page

--- ajax/printCard.php
+++ ajax/printCard.php
@@ -47,6 +47,17 @@ try {
       'middle_name' => ['C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','C13','C14','CX'],
       'birthday' => ['D1','D2','D3','D4','D5','D6','D7','D8'],
     ];
+    $education = [
+        'Среднее профессиональное – квалифицированный рабочий, служащий' => 'V1',
+        'Среднее профессиональное – специалист среднего звена' => 'V2',
+        'Среднее профессиональное – студент (обучаюсь)' => 'V3',
+        'Высшее профессиональное – бакалавриат' => 'V4',
+        'Высшее профессиональное – специалитет' => 'V5',
+        'Высшее профессиональное – магистратура' => 'V6',
+        'Высшее профессиональное – аспирантура' => 'V7',
+        'Высшее профессиональное – докторантура' => 'V8',
+        'Высшее профессиональное – студент (обучаюсь)' => 'V9',
+    ];
     $fileInfo = \CFile::GetFileArray($student['UF_STATEMENT']);
     $phpWord = new PhpWord();
     $templateProcessor = new TemplateProcessor(__DIR__ . '/../local/templates/main/assets/word/Карточка-заявка.docx');
@@ -68,14 +79,21 @@ try {
     foreach ($name['birthday'] as $key => $value){
         $templateProcessor->setValue($value, $birthday[$key]);
     }
+    foreach ($education as $key => $value){
+        if ($key == $student['UF_EDUCATION']){
+            $templateProcessor->setValue($value, 'V');
+        }else{
+            $templateProcessor->setValue($value, '');
+        }
+    }
     $templateProcessor->setValue('AGE', $student['UF_YEARS']);
     $templateProcessor->setValue('CITIZENSHIP', $student['UF_CITIZENSHIP']);
     $templateProcessor->setValue('EDUCATION_ORGANISATION', $student['UF_CITIZENSHIP']);
     $templateProcessor->setValue('SERIA_DIPLOMA', $student['UF_SERIES']);
     $templateProcessor->setValue('DIPLOMA_DATE', ($student['UF_DIPLOMA_DATE'])->format('d.m.Y'));
     $templateProcessor->setValue('WORKPLACE', $student['UF_WORK_PLACE']);
-    $templateProcessor->setValue('AGRARIAN', $student['UF_AGRARIAN']);
-    $templateProcessor->setValue('POSITION', $student['UF_POSITION']? 'Да': 'Нет');
+    $templateProcessor->setValue('AGRARIAN', $student['UF_AGRARIAN']? 'Да': 'Нет');
+    $templateProcessor->setValue('POSITION', $student['UF_POSITION']);
     $templateProcessor->setValue('WORK_EXPERIENCE', $student['UF_EXPIRIANCE']);
     $templateProcessor->setValue('HOME_INDEX', $student['UF_ADDRESS']);
     $templateProcessor->setValue('PHONE', $student['UF_PHONE']);
